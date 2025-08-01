31888835475ea2f59c637bb52505e08d01778352
Archi <archivirani01@gmail.com>
2025-08-01 12:52:30 +0530

    Invoice Details

--- scorpion/src/app/components/docket-list/freight-details/freight-details.component.ts
+++ scorpion/src/app/components/docket-list/freight-details/freight-details.component.ts
@@ -7,5 +7,17 @@ import { Component } from '@angular/core';
   styleUrl: './freight-details.component.scss'
 })
 export class FreightDetailsComponent {
+toPayAmount: string = '0.00';
 
+  onFocus(): void {
+    if (this.toPayAmount === '0.00') {
+      this.toPayAmount = '';
+    }
+  }
+
+  onBlur(): void {
+    if (this.toPayAmount.trim() === '') {
+      this.toPayAmount = '0.00';
+    }
+  }
 }
