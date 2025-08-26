a978846d477de797a0b87d0580043d39dd371eb6
Archi <archivirani01@gmail.com>
2025-08-26 16:19:46 +0530

    validation add

--- scorpion/src/environments/environment.prod.ts
+++ scorpion/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     //  apiUrl: 'https://seplapi.cygnux.in/V1/',
-    apiUrl:'https://seplnewapiuat.cygnux.in/v1/',
+    apiUrl:'https://seplnewapiuat.cygnux.in/api/v1/',
     env: 'Prod'
 }
