40071a4dbf180c73b6b5ff3d138de19146b1a384
Archi <archivirani01@gmail.com>
2025-09-05 17:46:45 +0530

    dynamic location

--- scorpion/src/environments/environment.prod.ts
+++ scorpion/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     //  apiUrl: 'https://seplapi.cygnux.in/V1/',
-    apiUrl:'https://seplnewapiuat.cygnux.in/api/v2/',
+    apiUrl:'https://seplnewapiuat.cygnux.in/api/v1/',
     env: 'Prod'
 }
