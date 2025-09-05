7e3eed805ae8dfca8d902fcc8d90fe42b80eeb00
Archi <archivirani01@gmail.com>
2025-09-05 11:49:04 +0530

    change version

--- scorpion/src/environments/environment.prod.ts
+++ scorpion/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
     //  apiUrl: 'https://seplapi.cygnux.in/V1/',
-    apiUrl:'https://seplnewapiuat.cygnux.in/api/v1/',
+    apiUrl:'https://seplnewapiuat.cygnux.in/api/v1.1/',
     env: 'Prod'
 }
