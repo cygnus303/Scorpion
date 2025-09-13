6f031e30fc24e1e495e1add8da89d3c2e5019d09
Archi <archivirani01@gmail.com>
2025-09-13 12:46:11 +0530

    FUELSURCHARGE API CALL

--- scorpion/src/app/shared/services/docket.service.ts
+++ scorpion/src/app/shared/services/docket.service.ts
@@ -1063,7 +1063,7 @@ validateAppointmentDate() {
             }
           });
           this.subTotalCalculation();
-
+          this.getFuelSurcharge(this.freightData.freightCharge);
         }
       },
     });
