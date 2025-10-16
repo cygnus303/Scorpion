78648e07972b9c17c22c37700cb9b8be4ea5d8b9
Archi <archivirani01@gmail.com>
2025-10-16 12:56:16 +0530

    api implement

--- scorpion/src/app/shared/services/thc-master.service.ts
+++ scorpion/src/app/shared/services/thc-master.service.ts
@@ -2,7 +2,6 @@ import { Inject, Injectable } from '@angular/core';
 import { ApiHandlerService } from './api-handler.service';
 import { Observable } from 'rxjs';
 import { IApiBaseResponse } from '../interface/api-base-action-response';
-import { CityResponse, VendeorsResponse } from '../models/thc-master.model';
 
 @Injectable({
   providedIn: 'root'
@@ -13,10 +12,26 @@ export class THCMasterService {
   constructor(@Inject(ApiHandlerService) private apiHandlerService: ApiHandlerService) {}
 
   getVendorsList(vendors: any) {
-      return this.apiHandlerService.Post(`THC/GetVendors`,vendors);
-    }
+    return this.apiHandlerService.Post(`THC/GetVendors`,vendors);
+  }
 
-    getCityList(): Observable<any> {
-      return this.apiHandlerService.Get(`Operation/GetCity`);
-    }
+  getCityList(): Observable<any> {
+    return this.apiHandlerService.Get(`Operation/GetCity`);
+  }
+
+  getTripSheet(vehicleNo:string): Observable<IApiBaseResponse<any[]>>{
+    return this.apiHandlerService.Get(`THC/TripSheetFromVehicle?vehicleNo=${vehicleNo}`);
+  }
+
+  getVahicleCapacity(id:string): Observable<IApiBaseResponse<any[]>>{
+    return this.apiHandlerService.Get(`THC/VehicleCapacity?id=${id}`);
+  }
+
+  getPANnumber(vendor:any): Observable<IApiBaseResponse<any[]>>{
+    return this.apiHandlerService.Get(`THC/GetPanNoFromVendor?vendor=${vendor}`);
+  }
+
+  getvehicleDetailFromVendor(vendorType:string,vendor:string): Observable<IApiBaseResponse<any[]>>{
+    return this.apiHandlerService.Get(`THC/VehicleFromVendor?vendorType=${vendorType}&vendor=${vendor}`);
+  }
 }
