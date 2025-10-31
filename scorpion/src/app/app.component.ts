cc4764d1f01112e7279e41020cb690f968744039
Archi <archivirani01@gmail.com>
2025-10-31 16:59:49 +0530

    validation add

--- scorpion/src/app/app.component.ts
+++ scorpion/src/app/app.component.ts
@@ -22,35 +22,17 @@ export class AppComponent {
   
   title = 'scorpion';
 
-  // public loading: string = 'disable';
   constructor(public commonService: CommonService,public spinner: NgxSpinnerService,public docketService: DocketService){
      const saved = localStorage.getItem("loginUserList");
     if (saved) {
       this.docketService.loginUserList = JSON.parse(saved);
-      this.docketService.loginUserList.Type = '3'
+      this.docketService.loginUserList.Type = '2'
       this.docketService.loginUserList.LocationCode =  'PIM';
       this.docketService.Location = this.docketService.loginUserList.LocationCode;
       this.docketService.isComplition = false;
       this.docketService.BaseUserCode = this.docketService.loginUserList.UserId;
       this.docketService.baseUsername = this.docketService.loginUserList.BaseUserName;
     }
-    // this.commonService.isLoading.subscribe({
-    //   next: (response) => {
-    //     setTimeout(()=>{
-    //       if (response != null) {
-    //         this.loading = response ? 'enable' : 'disable';
-    //       }
-    //     },500)
-    //   },
-    //   error: (response: any) => {},
-    // });
   }
-  
-  // ngOnInit() {
-  //   this.spinner.show();
-  //   setTimeout(() => {
-  //     this.spinner.hide();
-  //   }, 5000);
-  // }
 
 }
