4369988f1c1922930e1521a6aedbab64e8d2879a
vidhi Gajera <vidhigajera2004@gmail.com>
2025-11-28 16:29:11 +0530

    loading-SheetLIst

--- scorpion/src/app/components/loading-sheet/loading-sheet.component.ts
+++ scorpion/src/app/components/loading-sheet/loading-sheet.component.ts
@@ -1,14 +1,95 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-loading-sheet',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,NgSelectModule],
   templateUrl: './loading-sheet.component.html',
   styleUrls: ['./loading-sheet.component.scss']
 })
 export class LoadingSheetComponent {
+LoadingSheetList = [
+  {
+    "CnoteNo": "62970933",
+    "ManualCnoteNo": "62970933",
+    "BkgDate": "31 Oct 25",
+    "TransMode": "ROAD CARGO",
+    "BkgLoc": "PIM",
+    "DelyLoc": "CNI",
+    "CityFromTo": "PIMPLAS - CHENNAI",
+    "CommDelyDate": "03 Nov 2025",
+    "Rate": "PER KG",
+    "Pkgs_LB": 10,
+    "Wt_LB": 120
+  },
+  {
+    "CnoteNo": "62970934",
+    "ManualCnoteNo": "62970934",
+    "BkgDate": "31 Oct 25",
+    "TransMode": "ROAD CARGO",
+    "BkgLoc": "PIM",
+    "DelyLoc": "AMD",
+    "CityFromTo": "PIMPLAS - AHMEDABAD",
+    "CommDelyDate": "03 Nov 2025",
+    "Rate": "PER KG",
+    "Pkgs_LB": 10,
+    "Wt_LB": 140
+  },
+  {
+    "CnoteNo": "62970935",
+    "ManualCnoteNo": "62970935",
+    "BkgDate": "31 Oct 25",
+    "TransMode": "ROAD CARGO",
+    "BkgLoc": "PIM",
+    "DelyLoc": "KUMR",
+    "CityFromTo": "PIMPLAS - BAMUNIMAIDAN",
+    "CommDelyDate": "09 Nov 2025",
+    "Rate": "PER KG",
+    "Pkgs_LB": 30,
+    "Wt_LB": 22
+  },
+  {
+    "CnoteNo": "62970936",
+    "ManualCnoteNo": "62970936",
+    "BkgDate": "06 Nov 25",
+    "TransMode": "ROAD CARGO",
+    "BkgLoc": "PIM",
+    "DelyLoc": "AMD",
+    "CityFromTo": "PIMPLAS - AHMEDABAD",
+    "CommDelyDate": "10 Nov 2025",
+    "Rate": "PER KG",
+    "Pkgs_LB": 8,
+    "Wt_LB": 265
+  },
+  {
+    "CnoteNo": "62970937",
+    "ManualCnoteNo": "62970937",
+    "BkgDate": "06 Nov 25",
+    "TransMode": "ROAD CARGO",
+    "BkgLoc": "PIM",
+    "DelyLoc": "AMD",
+    "CityFromTo": "PIMPLAS - AHMEDABAD",
+    "CommDelyDate": "10 Nov 2025",
+    "Rate": "PER KG",
+    "Pkgs_LB": 2,
+    "Wt_LB": 120
+  },
+  {
+    "CnoteNo": "62970938",
+    "ManualCnoteNo": "62970938",
+    "BkgDate": "06 Nov 25",
+    "TransMode": "ROAD CARGO",
+    "BkgLoc": "PIM",
+    "DelyLoc": "JAI",
+    "CityFromTo": "PIMPLAS - JAIPUR",
+    "CommDelyDate": "10 Nov 2025",
+    "Rate": "PER KG",
+    "Pkgs_LB": 2,
+    "Wt_LB": 20
+  }
+]
 
 }
