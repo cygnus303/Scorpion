724f86a17ff9929b2225080116d7a2447cce6a5a
Archi <archivirani01@gmail.com>
2025-11-28 16:37:14 +0530

    api implemented

--- scorpion/src/app/components/loading-sheet/loading-sheet.component.ts
+++ scorpion/src/app/components/loading-sheet/loading-sheet.component.ts
@@ -1,14 +1,43 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { ChallanService } from 'app/shared/services/challan.service';
+import { CommonService } from 'app/shared/services/common.service';
+import { DocketService } from 'app/shared/services/docket.service';
+import { GeneralMasterService } from 'app/shared/services/general-master.service';
+import { LoadingSheetService } from 'app/shared/services/loading-sheet.service';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-loading-sheet',
   standalone: true,
-  imports: [CommonModule,RouterModule],
+  imports: [CommonModule,RouterModule,ReactiveFormsModule,NgSelectModule,BsDatepickerModule],
   templateUrl: './loading-sheet.component.html',
   styleUrls: ['./loading-sheet.component.scss']
 })
 export class LoadingSheetComponent {
+  public loadingName:string='';
+  constructor(
+    public loadingSheetService:LoadingSheetService,
+    public generalMasterService:GeneralMasterService,
+    public docketService:DocketService,
+    public commonService:CommonService,
+    public challanService:ChallanService 
+  ){}
 
+ngOnInit(){
+      this.docketService.loginUserList.LocationCode =  'PIM';
+    this.loadingSheetService.buildForm();
+    // this.generalMasterService.getLoadingByDetail();
+    this.generalMasterService.getModeData();
+    this.docketService.getServiceTypeData(1);
+    this.generalMasterService.getChargeTypeData();
+  }
+  
+  getvendoCodeData(event:any){
+    this.loadingName=event?.codeDesc
+    this.challanService.branchWiseLoadingUnloading(event?.codeId)
+  }
 }
---
+++ scorpion/src/app/shared/services/loading-sheet.service.ts
@@ -0,0 +1,52 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DocketService } from './docket.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoadingSheetService {
+  LSForm!:FormGroup;
+
+  constructor(public docketService:DocketService) { }
+
+
+  buildForm(){
+    const today = new Date();
+  const fromDate = new Date();
+  fromDate.setDate(today.getDate() - 29);
+
+    this.LSForm=new FormGroup({
+      LsNO:new FormControl(''),
+      LsDate:new FormControl(this.formatDate(today)),
+      ManualLsNO:new FormControl('N/A'),
+      LoadingBy:new FormControl(null,[Validators.required]),
+      NextStopLocation:new FormControl(null,[Validators.required]),
+      RateType:new FormControl(null,[Validators.required]),
+      MF_TransportMode:new FormControl('6',[Validators.required]),
+      rdVehicle:new FormControl('Own'),
+      SealNo:new FormControl(''),
+      VEHNO:new FormControl(null),
+      PreparedBy:new FormControl(''),
+      UserName:new FormControl(this.docketService.baseUsername),
+      LSType:new FormControl(null),
+      reportrange:new FormControl([fromDate, today]),
+      TransportMode:new FormControl(null),
+      DestinationList:new FormControl(''),
+      DocketNoList:new FormControl(''),
+      VendorCode:new FormControl(null),
+      VendorName:new FormControl(''),
+      LoadingCharge:new FormControl(''),
+      IsMathadi:new FormControl(''),
+      MathadiSlipNo:new FormControl(''),
+      MathadiDate:new FormControl(''),
+      MathadiAmt:new FormControl('')
+    })
+  }
+  formatDate(date: Date): string {
+  const day = date.getDate();
+  const month = date.toLocaleString('en-US', { month: 'long' }); // November
+  const year = date.getFullYear();
+  return `${day} ${month} ${year}`;
+}
+}
