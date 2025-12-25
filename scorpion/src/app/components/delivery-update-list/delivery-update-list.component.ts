fd8ab011cf1b84fd2da8f1a08d0ae38eaaf63361
vidhi Gajera <vidhigajera2004@gmail.com>
2025-12-25 17:31:43 +0530

    added calculation

--- scorpion/src/app/components/delivery-update-list/delivery-update-list.component.ts
+++ scorpion/src/app/components/delivery-update-list/delivery-update-list.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { FormArray, FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { BranchWiseLoadingUnloading } from 'app/shared/models/thc-master.model';
 import { ChallanService } from 'app/shared/services/challan.service';
 import { DocketService } from 'app/shared/services/docket.service';
 import { GeneralMasterService } from 'app/shared/services/general-master.service';
@@ -18,6 +19,7 @@ import { THCMasterService } from 'app/shared/services/thc-master.service';
 export class DeliveryUpdateListComponent {
   public DRSSummaryForm!:FormGroup;
   public DRSInformation!:any;
+  public branchWiseLoadingUnloadingList:BranchWiseLoadingUnloading[]=[];
 
   constructor(public challanService:ChallanService,public docketService:DocketService,private THCService:THCMasterService,public generalMasterService:GeneralMasterService){}
 
@@ -36,7 +38,7 @@ buildForm(){
     Rate:new FormControl(null),
     closeKM:new FormControl(0),
     ratetype:new FormControl(null),
-    drsList: new FormArray([]) 
+    drsList: new FormArray([])
   })
 }
 
@@ -44,34 +46,70 @@ get drsList(): FormArray {
   return this.DRSSummaryForm.get('drsList') as FormArray;
 }
 
-createDrsRow(item: any): FormGroup {
-  return new FormGroup({
-    autoNo: new FormControl(item.autoNo),
-    dockno: new FormControl(item.dockno),
-    booking_Date: new FormControl(item.booking_Date),
-    orgncd: new FormControl(item.orgncd),
-    destcd: new FormControl(item.destcd),
-    payBasis: new FormControl(item.payBasis),
-    csgncd: new FormControl(item.csgncd),
-    csgnnm: new FormControl(item.csgnnm),
-    csgecd: new FormControl(item.csgecd),
-    csgenm: new FormControl(item.csgenm),
-    actQty:new FormControl(item.actQty),
-    pkgQty:new FormControl(item.pkgQty),
-    pkgs_Pending: new FormControl(item.pkgs_Pending),
-    pkgs_Arrived: new FormControl(item.pkgs_Arrived),
-    pkgs_Booked: new FormControl(item.pkgs_Booked),
-    comm_Dely_Dt: new FormControl(item.comm_Dely_Dt),
-    deliveredPkgs: new FormControl(item.pkgs_Arrived),
-    remarks: new FormControl(''),
-    isChecked: new FormControl(item.isChecked),
-    isBadPod: new FormControl(item.isEnabledBadPodoption),
-    ratetype: new FormControl(item.rateType),  
-    newRate: new FormControl(item.rate),
-    otp: new FormControl('')
-  });
+  createDrsRow(data: any[]) {
+    data.forEach((item) => {
+      const group = new FormGroup({
+        autoNo: new FormControl(item.autoNo),
+        dockno: new FormControl(item.dockno),
+        booking_Date: new FormControl(item.booking_Date),
+        orgncd: new FormControl(item.orgncd),
+        destcd: new FormControl(item.destcd),
+        payBasis: new FormControl(item.payBasis),
+        csgncd: new FormControl(item.csgncd),
+        csgnnm: new FormControl(item.csgnnm),
+        csgecd: new FormControl(item.csgecd),
+        csgenm: new FormControl(item.csgenm),
+        actQty: new FormControl(item.actQty),
+        pkgQty: new FormControl(item.pkgQty),
+        pkgs_Pending: new FormControl(item.pkgs_Pending),
+        pkgs_Arrived: new FormControl(item.pkgs_Arrived),
+        pkgs_Booked: new FormControl(item.pkgs_Booked),
+        comm_Dely_Dt: new FormControl(item.comm_Dely_Dt),
+        deliveredPkgs: new FormControl(item.pkgs_Arrived),
+        remarks: new FormControl(''),
+        isChecked: new FormControl(item.isChecked),
+        isBadPod: new FormControl(item.isEnabledBadPodoption),
+        ratetype: new FormControl(item.rateType),
+        newRate: new FormControl(item.rate),
+        otp: new FormControl(''),
+        totalLoadingCharge: new FormControl(''),
+      });
+      group.get('ratetype')?.valueChanges.subscribe(() => this.calculateCharge(group));
+      group.get('newRate')?.valueChanges.subscribe(() => this.calculateCharge(group));
+      this.drsList.push(group);
+    });
+  }
+
+  calculateCharge(group: FormGroup) {
+  const rateType = group.get('ratetype')?.value;
+  const newRate = parseFloat(group.get('newRate')?.value || 0);
+  const actuwt = parseFloat(group.get('actQty')?.value || 0);
+  const pkgsno = parseFloat(group.get('pkgQty')?.value || 0);
+  let charge = 0;
+  switch (rateType) {
+    case '1': // PER KG
+      charge = actuwt * newRate;
+      break;
+    case '3': // PER PACKAGES
+      charge = pkgsno * newRate;
+      break;
+    case '4': // FLAT
+      charge = newRate;
+      break;
+    default:
+      charge = 0;
+  }
+  group.get('totalLoadingCharge')?.setValue(charge.toFixed(2), { emitEvent: false });
+  this.updateTotalLoadingCharge()
 }
 
+updateTotalLoadingCharge() {
+   const total = this.drsList.controls.reduce((sum, ctrl) => {
+      return sum + parseFloat(ctrl.get('totalLoadingCharge')?.value || 0);
+  }, 0);
+
+  this.DRSSummaryForm.get('LoadingCharge')?.setValue(total.toFixed(2), { emitEvent: false });
+}
 // in TS
 getRadioControl(i: number): FormControl {
   return (this.drsList.at(i) as FormGroup).get('isBadPod') as FormControl;
@@ -95,8 +133,8 @@ getDeliveryDetail() {
       this.drsList.clear();
       docketList.forEach((item: any) => {
         item.rateType = summaryRateType; 
-        this.drsList.push(this.createDrsRow(item));
       });
+      this.createDrsRow(docketList);
       this.getPANnumberData(response.data.drsSummary.loadingBy);
     },
     error: (err) => {
@@ -105,20 +143,82 @@ getDeliveryDetail() {
   });
 }
 
+getLoadingCharge(event: any) {
+  const data = {
+    loadUnloadType: 'U',
+    vendorCode: event,
+    typeModule: this.docketService.loginUserList.Type === "2" ? "P" : "D",
+    chargeType: this.docketService.loginUserList.chargeType,
+    brdc: this.docketService.loginUserList.LocationCode,
+    loadingBy: this.DRSSummaryForm.value.LoadingBy,
+  };
+if(['XX5'].includes(this.DRSSummaryForm.get('LoadingBy')?.value)){
+  this.THCService.getLoadingCharge(data).subscribe({
+    next: (response: any) => {
+    this.DRSSummaryForm.patchValue({
+        Rate:response.rate
+      });
+      this.drsList.controls.forEach((item: any, index) => {
+        this.drsList.controls[index].patchValue({
+          newRate: response.rate,
+        });
+      });
+    },
+    error: (err) => {
+      console.error('Error fetching loading charge:', err);
+    }
+  });
+}
+}
+
 getPANnumberData(vendorCode:any){
    const ChargedBy = vendorCode;
     if (ChargedBy === 'B' || ChargedBy == '04') {
-      this.challanService.getChargesVendorsList('04');
+      this.getChargesVendorsList('04');
     }
     if (ChargedBy === 'A' || ChargedBy == 'XX1') {
-      this.challanService.getChargesVendorsList('XX1');
+      this.getChargesVendorsList('XX1');
     }
     if (ChargedBy === 'M') {
-      this.challanService.getChargesVendorsList('19');
+      this.getChargesVendorsList('19');
     }
     if (ChargedBy === 'XX5' || ChargedBy === 'XX8') {
-      this.challanService.branchWiseLoadingUnloading(vendorCode);
+      this.branchWiseLoadingUnloading(vendorCode);
     }
 }
+
+  branchWiseLoadingUnloading(event: any) {
+    const data = {
+      vendorType: event,
+      baseLocationCode: this.docketService.loginUserList.LocationCode,
+      type: 'U',
+    }
+    this.THCService.getBranchWiseLoadingUnloadingVendorList(data).subscribe({
+      next: (response) => {
+        if (response.success) {
+          this.branchWiseLoadingUnloadingList = response.data;
+        }
+      },
+    });
+  }
+
+  getChargesVendorsList(event: any) {
+    const data = {
+      vendorType: event?.codeId ? event?.codeId : event,
+      branchCode: this.docketService.loginUserList.LocationCode,
+      userName: this.docketService.loginUserList.BaseUserName,
+      documentType: this.docketService.loginUserList.Type
+    }
+    this.THCService.getVendorsList(data).subscribe({
+      next: (response) => {
+        if (response.success) {
+          this.branchWiseLoadingUnloadingList = response.data.map((x: any) => ({
+            value: x.vendor_Code,
+            text: x.vendor_Name
+          }));
+        }
+      },
+    });
+  }
 
 }
