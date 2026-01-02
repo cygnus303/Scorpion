98f977143a3124669f5d3bb90f89acc2691264ab
vidhi Gajera <vidhigajera2004@gmail.com>
2026-01-02 12:43:53 +0530

    set arrival and stock update

--- scorpion/src/app/components/arrival-stock-update-list/arrival-stock-update-list.component.ts
+++ scorpion/src/app/components/arrival-stock-update-list/arrival-stock-update-list.component.ts
@@ -20,7 +20,8 @@ export class ArrivalStockUpdateListComponent {
       // this.docketService.loginUserList.LocationCode =  'PIM';
       // this.docketService.loginUserList.loadBy = "XX9";
       // this.docketService.loginUserList.chargeType='1';
-      // this.docketService.loginUserList.id='VH/BWH/2526/002424';
+      // this.docketService.loginUserList.id='VH/LTT/2526/001764';
+      // this.docketService.loginUserList.Type = '1';
       this.docketService.BaseUserCode = this.docketService.loginUserList.UserId;
       this.docketService.baseUsername = this.docketService.loginUserList.BaseUserName;
     }
