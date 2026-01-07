bd8f13d4b1f639d2263d9fc825afd514a0de75d8
Archi <archivirani01@gmail.com>
2026-01-07 17:23:37 +0530

    component create

---
+++ scorpion/src/app/components/default-contract/default-contract.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-default-contract',
+  standalone: true,
+  imports: [],
+  templateUrl: './default-contract.component.html',
+  styleUrl: './default-contract.component.scss'
+})
+export class DefaultContractComponent {
+
+}
