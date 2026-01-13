0d3d2b102ac4bb3819cfb48f30f8a4985759b317
Archi <archivirani01@gmail.com>
2026-01-13 14:30:05 +0530

    implemented rate api

---
+++ scorpion/src/app/shared/services/default-contract.service.ts
@@ -0,0 +1,16 @@
+import { Inject, Injectable } from '@angular/core';
+import { ApiHandlerService } from './api-handler.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DefaultContractService {
+
+  constructor(@Inject(ApiHandlerService) private apiHandlerService: ApiHandlerService) { }
+
+
+  calculateRate(payload:any){
+    return this.apiHandlerService.Post(`Docket/CalculateRate`,payload);
+  }
+
+}
