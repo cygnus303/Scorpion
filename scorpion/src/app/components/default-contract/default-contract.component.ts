97a5f41f57e703a81c48a9465dafcd986b28a789
vidhi Gajera <vidhigajera2004@gmail.com>
2026-01-15 14:08:17 +0530

    changes  Control Name

--- scorpion/src/app/components/default-contract/default-contract.component.ts
+++ scorpion/src/app/components/default-contract/default-contract.component.ts
@@ -3,6 +3,7 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { BasicDetailService } from 'app/shared/services/basic-detail.service';
 import { DefaultContractService } from 'app/shared/services/default-contract.service';
 import { DocketService } from 'app/shared/services/docket.service';
+import { debounceTime, distinctUntilChanged, map } from 'rxjs';
 
 @Component({
   selector: 'default-contract',
@@ -14,11 +15,7 @@ export class DefaultContractComponent {
   public getPincodeMaster: any;
   public DefaultcontractForm!:FormGroup;
 
-  constructor(
-    public docketService:DocketService,
-    public basicDetailService:BasicDetailService,
-    public defaultContractService:DefaultContractService
-  ){}
+  constructor(public docketService:DocketService,public basicDetailService:BasicDetailService,public defaultContractService:DefaultContractService){}
 
   ngOnInit(){
     this.buildForm();
@@ -33,7 +30,7 @@ export class DefaultContractComponent {
       originCity:new FormControl(''),
       fromState:new FormControl(''),
       originZone:new FormControl(''),
-      ODA_pickUp:new FormControl(''),
+      schG07:new FormControl(''),
       email:new FormControl('',[Validators.required, Validators.email]),
       destination_pincode:new FormControl(null, Validators.required),
       destinationArea:new FormControl(''),
@@ -44,9 +41,9 @@ export class DefaultContractComponent {
       deliveryBranchCode:new FormControl(''),
       ODACategory:new FormControl(''),
       mode:new FormControl(null),
-      TATNormal:new FormControl(''),
-      TATODA:new FormControl(''),
-      transitDays:new FormControl(''),
+      tatNormal:new FormControl(''),
+      tatoda:new FormControl(''),
+      trDays:new FormControl(''),
       weightKG:new FormControl(0, Validators.required),
       Pkgs:new FormControl(0, Validators.required),
       VolumetricAppl:new FormControl(false),
@@ -54,33 +51,47 @@ export class DefaultContractComponent {
       CSDDelivery:new FormControl(false),
       MallDelAppl:new FormControl(false),
       invoiceValue:new FormControl(0, Validators.required),
-      chargeWeightKG:new FormControl(0),
-      CFTweight:new FormControl(0),
-      rate:new FormControl(0),
+      chrgwt:new FormControl(0),
+      cftTotal:new FormControl(0),
+      freightRate:new FormControl(0),
       length:new FormControl(''),
       breadth:new FormControl(''),
       height:new FormControl(''),
       CFTRatio:new FormControl(''),
-      fuelSurcharge:new FormControl('',Validators.required),
-      freightRs:new FormControl(0),
+      schG20:new FormControl('',Validators.required),
+      freightCharge:new FormControl(0),
       fuelSurchargeRs:new FormControl(0),
       ODARate:new FormControl(0),
-      gst:new FormControl(0),
+      gstRate:new FormControl(0),
       stateChargesDetail:new FormControl(0),
       stateCharges:new FormControl(0),
-      ODAAmount:new FormControl(0),
-      docketCharge:new FormControl(''),
-      mallDeliveryCharge:new FormControl(0),
-      appoinmentCharge:new FormControl(0),
-      CSDCharge:new FormControl(0),
-      InsuranceCharge:new FormControl(0),
+      schG08:new FormControl(0),
+      schG04:new FormControl(''),
+      schG17:new FormControl(0),
+      uchG08:new FormControl(0),
+      schG10:new FormControl(0),
+      ichG01:new FormControl(0),
       Disc_Rate:new FormControl(0),
       Disc_amount:new FormControl(0),
       Disc_Sub_Total:new FormControl(0),
       subTotal:new FormControl(0),
       GrandTotal:new FormControl(0)
-    })
+    });
+    this.listenDefaultContractChanges();
   }
+
+  listenDefaultContractChanges() {
+    this.DefaultcontractForm.valueChanges.pipe(debounceTime(500), map(form => ({
+      mode: form.mode,
+      invoiceValue: form.invoiceValue,
+      Pkgs: form.Pkgs,
+      originPincode: form.originPincode,
+      destinationPincode: form.destination_pincode
+    })),distinctUntilChanged((prev, curr) => JSON.stringify(prev) === JSON.stringify(curr))).subscribe(() => {
+        this.calculateRate();
+      });
+  }
+
 
   clearOnFocus(controlName: string) {
     const control = this.DefaultcontractForm.get(controlName);
@@ -123,49 +134,30 @@ export class DefaultContractComponent {
         }
       }
     });
-    this.calculateRate()
   }
 
-  calculateRate(){
-    const payload={
-    trnMode: this.DefaultcontractForm.value.mode,
-    contractID: "P018888",
-    invoiceAmount: this.DefaultcontractForm.value.invoiceValue || 0,
-    actualWeight: this.DefaultcontractForm.value.chargeWeightKG || 0,
-    totalCFT:  0,
-    packageCount: this.DefaultcontractForm.value.Pkgs || 0,
-    originPincode:this.DefaultcontractForm.value.originPincode,
-    destinationPincode: this.DefaultcontractForm.value.destination_pincode
-}
-
-if(!this.DefaultcontractForm.value.mode || !this.DefaultcontractForm.value.invoiceValue || 
- !this.DefaultcontractForm.value.Pkgs || !this.DefaultcontractForm.value.originPincode || !this.DefaultcontractForm.value.destination_pincode
-){
-  return;
-}
+  calculateRate() {
+    const payload = {
+      trnMode: this.DefaultcontractForm.value.mode,
+      contractID: "P018888",
+      invoiceAmount: this.DefaultcontractForm.value.invoiceValue || 0,
+      actualWeight: this.DefaultcontractForm.value.chargeWeightKG || 0,
+      totalCFT: 0,
+      packageCount: this.DefaultcontractForm.value.Pkgs || 0,
+      originPincode: this.DefaultcontractForm.value.originPincode,
+      destinationPincode: this.DefaultcontractForm.value.destination_pincode
+    }
+    if (!payload.trnMode || !payload.invoiceAmount || !payload.packageCount || !payload.originPincode || !payload.destinationPincode) {
+      return;
+    }
     this.defaultContractService.calculateRate(payload).subscribe({
       next: (response: any) => {
         if (response) {
-          console.log(response);
-          debugger
-          const csdCharge = response.otherCharges?.find((x: any) => x.chargecode === 'SCHG10')?.charge || 0;
-          const malldelivery=response.otherCharges?.find((x: any) => x.chargecode === 'SCHG17')?.charge || 0;
-          const appoinment=response.otherCharges?.find((x: any) => x.chargecode === 'UCHG08')?.charge || 0;
-          const fuelSurcharge=response.otherCharges?.find((x: any) => x.chargecode === 'SCHG20')?.charge || 0;
-          this.DefaultcontractForm.patchValue({
-            freightRs:response.freight.freightCharge,
-            chargeWeightKG:response.freight.chrgwt,
-            CSDCharge:csdCharge,
-            mallDeliveryCharge:malldelivery,
-            appoinmentCharge:appoinment,
-        
-          })
-          
+          this.DefaultcontractForm.patchValue(response);
         }
-
-        }
-      })
-    }
+      }
+    })
+  }
 
   OnSubmit(){
     if(this.DefaultcontractForm.valid){
