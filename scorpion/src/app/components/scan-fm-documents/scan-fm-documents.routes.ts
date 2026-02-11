1374ea3fdd38a71f7f582a36353743292fe042ce
Archi <archivirani01@gmail.com>
2026-02-11 15:18:08 +0530

    forward-document compoennt create

--- scorpion/src/app/components/scan-fm-documents/scan-fm-documents.routes.ts
+++ scorpion/src/app/components/scan-fm-documents/scan-fm-documents.routes.ts
@@ -1,12 +1,17 @@
 import { Routes } from '@angular/router';
 import { ScanFMDocumentsComponent } from './scan-fm-documents.component';
 import { DocketGuard } from 'app/shared/docket.guard';
+import { ForwardDocumentsComponent } from './forward-documents/forward-documents.component';
 
 export const ScanFMRoutes: Routes = [
   {
     path: 'ScanFMDocuments',
     component: ScanFMDocumentsComponent,
     canActivate: [DocketGuard]
+  },
+    {
+    path: 'ForwardFMDocumentsQuery',
+    component: ForwardDocumentsComponent,
   },
 
 ];
