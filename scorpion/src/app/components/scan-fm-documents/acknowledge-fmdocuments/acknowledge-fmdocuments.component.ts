8650a7878c85e013242523911f887329826d07b8
vidhi Gajera <vidhigajera2004@gmail.com>
2026-02-12 15:36:27 +0530

    implemented from validation

--- scorpion/src/app/components/scan-fm-documents/acknowledge-fmdocuments/acknowledge-fmdocuments.component.ts
+++ scorpion/src/app/components/scan-fm-documents/acknowledge-fmdocuments/acknowledge-fmdocuments.component.ts
@@ -2,17 +2,52 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { Router } from '@angular/router';
-
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { CommonService } from 'app/shared/services/common.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 @Component({
   selector: 'app-acknowledge-fmdocuments',
   standalone: true,
-  imports: [CommonModule , NgSelectModule],
+  imports: [CommonModule, NgSelectModule, BsDatepickerModule, ReactiveFormsModule],
   templateUrl: './acknowledge-fmdocuments.component.html',
   styleUrl: './acknowledge-fmdocuments.component.scss'
 })
 export class AcknowledgeFMDocumentsComponent {
- constructor(private router: Router) { }
- goToForwardList() {
-    this.router.navigate(['/Document/ForwardFMAckDocuments']);
+  public acknowledgefilterForm!: FormGroup;
+  public DocTypelist = [
+    { text: "Bill", value: "2" },
+    { text: "COD/DOD", value: "4" },
+    { text: "POD", value: "1" },
+    { text: "THC", value: "6" },
+  ];
+  public DocToList = [
+    { text: "Customer", value: "1" },
+    { text: "Location", value: "2" },
+  ];
+  
+  constructor(private router: Router, public commonService: CommonService) { }
+
+  ngOnInit() {
+    this.buildForm();
+  }
+
+  buildForm() {
+    const endDate = new Date(); // aaje ni date
+    const startDate = new Date();
+    startDate.setDate(endDate.getDate() - 29);
+    this.acknowledgefilterForm = new FormGroup({
+      FMType: new FormControl(null, [Validators.required]),
+      Loccode: new FormControl(null, [Validators.required]),
+      FmNo: new FormControl(null),
+      dateRange: new FormControl([startDate, endDate])
+    })
+  }
+
+  goToForwardList() {
+    if (this.acknowledgefilterForm.valid) {
+      this.router.navigate(['/Document/ForwardFMAckDocuments'], { state: { filterData: this.acknowledgefilterForm.value } });
+    } else {
+      this.acknowledgefilterForm.markAllAsTouched();
+    }
   }
 }
--- scorpion/src/app/components/scan-fm-documents/forward-documents/forward-documents.component.ts
+++ scorpion/src/app/components/scan-fm-documents/forward-documents/forward-documents.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { Router } from '@angular/router';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { GeneralMasterService } from 'app/shared/services/general-master.service';
 import { THCMasterService } from 'app/shared/services/thc-master.service';
-import { generalMasterResponse } from 'app/shared/models/general-master.model';
 import { PRSGeneralMasterResponse } from 'app/shared/models/thc-master.model';
 import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 import { CommonService } from 'app/shared/services/common.service';
@@ -13,62 +11,55 @@ import { CommonService } from 'app/shared/services/common.service';
 @Component({
   selector: 'app-forward-documents',
   standalone: true,
-  imports: [CommonModule , NgSelectModule , ReactiveFormsModule,BsDatepickerModule],
+  imports: [CommonModule, NgSelectModule, ReactiveFormsModule, BsDatepickerModule],
   templateUrl: './forward-documents.component.html',
   styleUrl: './forward-documents.component.scss'
 })
 export class ForwardDocumentsComponent {
-  public filterForm!:FormGroup;
-  public PayBsData:PRSGeneralMasterResponse[]=[];
-  public DocTypelist =[
-    { text:"Bill", value : "2" },
-    { text:"COD/DOD", value : "4" },
-    { text:"POD", value : "1" },
-    { text:"THC", value : "6" },
+  public filterForm!: FormGroup;
+  public PayBsData: PRSGeneralMasterResponse[] = [];
+  public DocTypelist = [
+    { text: "Bill", value: "2" },
+    { text: "COD/DOD", value: "4" },
+    { text: "POD", value: "1" },
+    { text: "THC", value: "6" },
   ];
-  public DateTypelist =[
-    { text:"THC Date", value : "1" },
-    { text:"Arrival Date", value : "2" }
+  public DateTypelist = [
+    { text: "THC Date", value: "1" },
+    { text: "Arrival Date", value: "2" }
   ]
 
+  constructor(private router: Router,public THCMasterService: THCMasterService,public commonService: CommonService) { }
 
-
-  constructor(
-    private router: Router,
-    public THCMasterService:THCMasterService,
-    public commonService:CommonService
-  ) { }
-
-  ngOnInit(){
+  ngOnInit() {
     this.buildForm();
   }
 
-  buildForm(){
-     const endDate = new Date(); // aaje ni date
-  const startDate = new Date();
-  startDate.setDate(endDate.getDate() - 29); 
-    this.filterForm=new FormGroup({
-      DocType:new FormControl(null,[Validators.required]),
-      DT_TYPE:new FormControl(null,[Validators.required]),
-      Paybas:new FormControl(null,[Validators.required]),
-      Dockets:new FormControl(null),
-      dateRange:new FormControl([startDate, endDate])
-  })
+  buildForm() {
+    const endDate = new Date(); // aaje ni date
+    const startDate = new Date();
+    startDate.setDate(endDate.getDate() - 29);
+    this.filterForm = new FormGroup({
+      DocType: new FormControl(null, [Validators.required]),
+      DT_TYPE: new FormControl(null, [Validators.required]),
+      Paybas: new FormControl(null, [Validators.required]),
+      Dockets: new FormControl(null),
+      dateRange: new FormControl([startDate, endDate])
+    })
   }
 
-  onChangeDocType(event:any){
-    if(event?.value==='2'){
-      this.PayBsData=[];
+  onChangeDocType(event: any) {
+    if (event?.value === '2') {
+      this.PayBsData = [];
       this.getPaybsData('BILLTYP');
-    }else {
-      this.PayBsData=[];
+    } else {
+      this.PayBsData = [];
       this.getPaybsData('PAYTYP');
     }
   }
 
-  getPaybsData(codeType:string) {
-    this.THCMasterService.getGeneralMasterDetail(codeType).subscribe({
-      next: (response) => {
+  getPaybsData(codeType: string) {
+    this.THCMasterService.getGeneralMasterDetail(codeType).subscribe({ next: (response) => {
         if (response.success) {
           this.PayBsData = response.data;
         }
@@ -77,15 +68,10 @@ export class ForwardDocumentsComponent {
   }
 
   goToForwardList() {
-    if(this.filterForm.valid){
-      this.router.navigate(
-      ['/Document/ForwardFMDocuments'],
-      { state: { filterData: this.filterForm.value } }
-      );
-    }else{
+    if (this.filterForm.valid) {
+      this.router.navigate(['/Document/ForwardFMDocuments'],{ state: { filterData: this.filterForm.value } });
+    } else {
       this.filterForm.markAllAsTouched();
     }
   }
-
-
 }
