d75f62636ff11aa64318c760feae798397f61977
Archi <archivirani01@gmail.com>
2026-02-12 14:28:07 +0530

    folderdocument filter api

--- scorpion/src/app/components/scan-fm-documents/forward-documents/forward-document-list/forward-document-list.component.ts
+++ scorpion/src/app/components/scan-fm-documents/forward-documents/forward-document-list/forward-document-list.component.ts
@@ -1,14 +1,70 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { CommonService } from 'app/shared/services/common.service';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-forward-document-list',
   standalone: true,
-  imports: [CommonModule,NgSelectModule],
+  imports: [CommonModule,NgSelectModule,ReactiveFormsModule,BsDatepickerModule],
   templateUrl: './forward-document-list.component.html',
   styleUrl: './forward-document-list.component.scss'
 })
 export class ForwardDocumentListComponent {
+  public currentDateTime: string = '';
+  public forwardDocForm !:FormGroup;
+  public  DocToList = [
+    { text:"Customer", value : "1" },
+    { text:"Location", value : "2" },
+  ];
+
+  constructor(public commonService:CommonService){}
+
+
+ngOnInit() {
+   this.setCurrentDateTime();
+   this.buildForm();
+   this.commonService.dateAccess('58')
+  const data = history.state.filterData;
+  console.log('Received:', data);
+}
+
+buildForm(){
+  const data = history.state.filterData;
+  this.forwardDocForm=new FormGroup({
+    FM_No:new FormControl(null),
+    FM_Date:new FormControl(new Date()),
+    Manual_FM_No:new FormControl(null),
+    FM_Doc_Type:new FormControl(data.DocType),
+    Courier_Way_Bill_Date:new FormControl(new Date()),
+    Doc_FWD_To:new FormControl(null),
+    Courier_Code:new FormControl(null),
+    Loc_Cust_Code:new FormControl(null),
+    Courier_Way_Bill_No:new FormControl(null)
+  })
+}
+
+setCurrentDateTime() {
+  const now = new Date();
+
+  const day = now.getDate();
+  const month = now.getMonth() + 1;   // 👈 month first
+  const year = now.getFullYear();
+
+  let hours = now.getHours();
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+  hours = hours % 12 || 12;
+
+  this.currentDateTime =
+    `${month}/${day}/${year} ${hours}:${minutes}:${seconds} ${ampm}`;
+}
+
+
+
 
 }
--- scorpion/src/app/components/scan-fm-documents/forward-documents/forward-documents.component.ts
+++ scorpion/src/app/components/scan-fm-documents/forward-documents/forward-documents.component.ts
@@ -2,17 +2,90 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { NgSelectModule } from '@ng-select/ng-select';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { GeneralMasterService } from 'app/shared/services/general-master.service';
+import { THCMasterService } from 'app/shared/services/thc-master.service';
+import { generalMasterResponse } from 'app/shared/models/general-master.model';
+import { PRSGeneralMasterResponse } from 'app/shared/models/thc-master.model';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
+import { CommonService } from 'app/shared/services/common.service';
 
 @Component({
   selector: 'app-forward-documents',
   standalone: true,
-  imports: [CommonModule , NgSelectModule],
+  imports: [CommonModule , NgSelectModule , ReactiveFormsModule,BsDatepickerModule],
   templateUrl: './forward-documents.component.html',
   styleUrl: './forward-documents.component.scss'
 })
 export class ForwardDocumentsComponent {
-  constructor(private router: Router) { }
-  goToForwardList() {
-    this.router.navigate(['/Document/ForwardFMDocuments']);
+  public filterForm!:FormGroup;
+  public PayBsData:PRSGeneralMasterResponse[]=[];
+  public DocTypelist =[
+    { text:"Bill", value : "2" },
+    { text:"COD/DOD", value : "4" },
+    { text:"POD", value : "1" },
+    { text:"THC", value : "6" },
+  ];
+  public DateTypelist =[
+    { text:"THC Date", value : "1" },
+    { text:"Arrival Date", value : "2" }
+  ]
+
+
+
+  constructor(
+    private router: Router,
+    public THCMasterService:THCMasterService,
+    public commonService:CommonService
+  ) { }
+
+  ngOnInit(){
+    this.buildForm();
   }
+
+  buildForm(){
+     const endDate = new Date(); // aaje ni date
+  const startDate = new Date();
+  startDate.setDate(endDate.getDate() - 29); 
+    this.filterForm=new FormGroup({
+      DocType:new FormControl(null,[Validators.required]),
+      DT_TYPE:new FormControl(null,[Validators.required]),
+      Paybas:new FormControl(null,[Validators.required]),
+      Dockets:new FormControl(null),
+      dateRange:new FormControl([startDate, endDate])
+  })
+  }
+
+  onChangeDocType(event:any){
+    if(event?.value==='2'){
+      this.PayBsData=[];
+      this.getPaybsData('BILLTYP');
+    }else {
+      this.PayBsData=[];
+      this.getPaybsData('PAYTYP');
+    }
+  }
+
+  getPaybsData(codeType:string) {
+    this.THCMasterService.getGeneralMasterDetail(codeType).subscribe({
+      next: (response) => {
+        if (response.success) {
+          this.PayBsData = response.data;
+        }
+      }
+    });
+  }
+
+  goToForwardList() {
+    if(this.filterForm.valid){
+      this.router.navigate(
+      ['/Document/ForwardFMDocuments'],
+      { state: { filterData: this.filterForm.value } }
+      );
+    }else{
+      this.filterForm.markAllAsTouched();
+    }
+  }
+
+
 }
