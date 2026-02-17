22d379df937554f24f8b889bab39caf3f83cdd89
Archi <archivirani01@gmail.com>
2026-02-17 15:11:53 +0530

    sweetalert msg show

--- scorpion/src/app/shared/services/docket.service.ts
+++ scorpion/src/app/shared/services/docket.service.ts
@@ -757,6 +757,7 @@ getcontractservicecharge() {
             this.getStaxPaidBy();
           }else{
             this.isContarctService = true;
+            this.sweetAlertService.info('Please enter valid information');
             this.contractservicecharge = [];
             this.invoiceform.patchValue({
               cft_Ratio: 0
