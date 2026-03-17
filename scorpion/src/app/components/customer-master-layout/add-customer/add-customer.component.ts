5a6464bd9cf583da2c7ec1846e930005ba022325
vidhi Gajera <vidhigajera2004@gmail.com>
2026-03-17 11:31:08 +0530

    Customer Master ui

--- scorpion/src/app/components/customer-master-layout/add-customer/add-customer.component.ts
+++ scorpion/src/app/components/customer-master-layout/add-customer/add-customer.component.ts
@@ -1,11 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { NgSelectModule } from '@ng-select/ng-select';
+import { TooltipDirective } from "ngx-bootstrap/tooltip";
 
 @Component({
   selector: 'app-add-customer',
   standalone: true,
-  imports: [CommonModule,NgSelectModule],
+  imports: [CommonModule, NgSelectModule, RouterModule],
   templateUrl: './add-customer.component.html',
   styleUrl: './add-customer.component.scss'
 })
--- scorpion/src/app/components/customer-master-layout/biling-information/biling-information.component.ts
+++ scorpion/src/app/components/customer-master-layout/biling-information/biling-information.component.ts
@@ -1,9 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { NgSelectModule } from '@ng-select/ng-select';
 
 @Component({
   selector: 'app-biling-information',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgSelectModule],
   templateUrl: './biling-information.component.html',
   styleUrl: './biling-information.component.scss'
 })
