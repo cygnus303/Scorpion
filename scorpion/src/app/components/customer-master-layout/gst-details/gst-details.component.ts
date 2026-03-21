e98cf7c60b6692e7c45582c75dcfaf8e26328bb4
vidhi <vidhicygnux2004@gmail.com>
2026-03-21 16:42:31 +0530

    added kam-details ui

--- scorpion/src/app/components/customer-master-layout/gst-details/gst-details.component.ts
+++ scorpion/src/app/components/customer-master-layout/gst-details/gst-details.component.ts
@@ -1,24 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { NgSelectModule } from '@ng-select/ng-select';
 import { CustomerMasterService } from 'app/shared/services/customer-master.service';
 
 @Component({
   selector: 'app-gst-details',
   standalone: true,
-  imports: [FormsModule,ReactiveFormsModule,CommonModule],
+  imports: [FormsModule, ReactiveFormsModule, CommonModule, NgSelectModule],
   templateUrl: './gst-details.component.html',
   styleUrl: './gst-details.component.scss'
 })
 export class GstDetailsComponent {
 
- constructor(
-  public customerMasterService:CustomerMasterService
- ){}
+  constructor(
+    public customerMasterService: CustomerMasterService
+  ) { }
 
- ngOnInit() {
-  this.customerMasterService.buildGSTForm();
-}
+  ngOnInit() {
+    this.customerMasterService.buildGSTForm();
+  }
 
 
 }
