acafcef757a9c0c723ef64021c22f1a428220869
Archi <archivirani01@gmail.com>
2026-04-02 11:08:49 +0530

    thc compoennt create

---
+++ scorpion/src/app/components/thc-list/thc-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-thc-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './thc-list.component.html',
+  styleUrl: './thc-list.component.scss'
+})
+export class ThcListComponent {
+
+}
