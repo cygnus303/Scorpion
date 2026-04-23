73bed3f718e1d03af21489ad5b9258773d95c1c3
Archi <archivirani01@gmail.com>
2026-04-23 12:37:56 +0530

    KOL static remove

--- scorpion/src/app/components/docket-list/docket-list.component.ts
+++ scorpion/src/app/components/docket-list/docket-list.component.ts
@@ -36,7 +36,7 @@ export class DocketListComponent implements OnInit {
     if (saved) {
       this.docketService.loginUserList = JSON.parse(saved);
       this.docketService.Location = this.docketService.loginUserList.LocationCode;
-      this.docketService.Location = 'KOL';
+      // this.docketService.Location = 'KOL';
       this.docketService.isComplition = false;
       this.docketService.BaseUserCode = this.docketService.loginUserList.UserId;
       this.docketService.baseUsername = this.docketService.loginUserList.BaseUserName;
