4498fbebad054a16d37fe2163842d360ed93983f
Archi <archivirani01@gmail.com>
2026-06-06 11:02:48 +0530

    HCC edit page UI

--- scorpion/src/app/components/hcc-finacialedit-list/hcc-finacialedit-list.component.ts
+++ scorpion/src/app/components/hcc-finacialedit-list/hcc-finacialedit-list.component.ts
@@ -1,12 +1,35 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { PaginationComponent } from 'app/shared/components/pagination/pagination.component';
+import { BsDatepickerModule } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-hcc-finacialedit-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, NgSelectModule, BsDatepickerModule, FormsModule, PaginationComponent],
   templateUrl: './hcc-finacialedit-list.component.html',
   styleUrl: './hcc-finacialedit-list.component.scss'
 })
 export class HccFinacialeditListComponent {
+  public config = {
+    fromDateStr: new Date(),
+    toDateStr: new Date(),
+    statusFilter: 'All',
+    page: 1,
+    pageSize: 10,
+    totalRecords: 0,
+    totalPages: 1,
+    searchText: ''
+  };
+  statusList = [
+    { label: 'All Status', value: 'All' },
+    { label: 'Generated', value: 'Generated' },
+    { label: 'Arrived', value: 'Arrived' },
+    { label: 'Billed', value: 'Billed' },
+    { label: 'Cancelled', value: 'Cancelled' },
+    { label: 'HCC Generated', value: 'HCC Generated' }
+  ];
 
 }
