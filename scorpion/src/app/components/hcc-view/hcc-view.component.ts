daffa1045f0e0c6f33703efcc051adff05cc1309
Archi <archivirani01@gmail.com>
2026-06-12 11:42:30 +0530

    laoding sheett hcc show

--- scorpion/src/app/components/hcc-view/hcc-view.component.ts
+++ scorpion/src/app/components/hcc-view/hcc-view.component.ts
@@ -29,7 +29,7 @@ export class HccViewComponent {
     const payload = {
       "FilterJson": {
         "ReportId": "364",
-        "Docno": data.pdcno ||data.drsNo,
+        "Docno": data.pdcno ||data.drsNo||data.mfNo,
         "DocType": flag,
         "ChargeType": chargeType
       }
