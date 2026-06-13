40717fda4b826ed537b2268ea520c326a23e363e
Archi <archivirani01@gmail.com>
2026-06-13 16:35:08 +0530

    THC detail show

--- scorpion/src/app/components/thc-edit/thc-edit.component.ts
+++ scorpion/src/app/components/thc-edit/thc-edit.component.ts
@@ -2,35 +2,26 @@ import { CommonModule } from '@angular/common';
 import { Component, TemplateRef, ViewChild } from '@angular/core';
 import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 import { PRSDRSApiService } from 'app/shared/services/prsdrs-api.service';
-
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-thc-edit',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './thc-edit.component.html',
   styleUrl: './thc-edit.component.scss'
 })
 export class ThcEditComponent {
     public modalRef!: BsModalRef;
-  
+    public selectedTHC:string='';
+    public thcData:any; 
     @ViewChild('Templatepod', { static: true }) Templatepod!: TemplateRef<any>;
-    public thcEditData=[
-      {
-        contractAmt:'35000',
-        StandardAmt :'0.00',
-        AdvanceAmt:'0',
-        advanceAmtPaid:'PIM',
-        balanceAmtAt:'PIM'
-      }
-    ]
   
+  constructor(private modalService: BsModalService,public PRSDRSApiService:PRSDRSApiService) { }
 
-  
-    constructor(private modalService: BsModalService,public PRSDRSApiService:PRSDRSApiService) { }
-
-      showPopup(data: any) {
+  showPopup(data: any) {
     console.log("HCC Details Data:", data);
+    this.selectedTHC=data.thcNo;
     this.getTHCEditDetail(data);
     this.modalRef = this.modalService.show(this.Templatepod, { class: 'modal-xl modal-dialog-centered', backdrop: true });
   }
@@ -41,20 +32,15 @@ export class ThcEditComponent {
       vendorType:null
     }
 
-    // this.PRSDRSApiService.getTHCList(params).subscribe({
-    //   next : (response:any)=>{
-    //     if(response){
-    //       this.thcData= response.data;
-    //        this.pagination.totalRecords = response.pagination.totalRecords;
-    //       this.pagination.totalPages = response.pagination.totalPages;
-    //       this.summaryData = response.summary;
-    //       this.isLoading = false;
-    //     }
-    //   }, error: (err) => {
-    //     console.error(err);
-    //     this.isLoading = false;
-    //   }
-    // })
+    this.PRSDRSApiService.getTHCEditDetail(params).subscribe({
+      next : (response:any)=>{
+        if(response){
+          this.thcData= response.thcsumry;
+        }
+      }, error: (err) => {
+        console.error(err);
+      }
+    })
 
 
 
