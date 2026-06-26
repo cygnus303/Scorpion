3588eb6d1270be607660e339f9218fa1d74b10a7
Archi <archivirani01@gmail.com>
2026-06-26 12:42:11 +0530

    remove api call stop issue

--- scorpion/src/app/components/stock-update-layout/stock-update-layout.component.ts
+++ scorpion/src/app/components/stock-update-layout/stock-update-layout.component.ts
@@ -104,17 +104,10 @@ export class StockUpdateLayoutComponent {
       statusFilter:this.config.statusFilter
     };
 
-    // const cacheKey = JSON.stringify(payload);
-    // if (this.apiCache.has(cacheKey)) {
-    //   this.handleApiResponse(this.apiCache.get(cacheKey));
-    //   return;
-    // }
-
     this.isLoading = true;
     this.listSubscription = this.stockUpdateService.getStockUpdateListing(payload).subscribe({
       next: (response: any) => {
         this.isLoading = false;
-        // this.apiCache.set(cacheKey, response);
         this.handleApiResponse(response);
       },
       error: (err: any) => {
