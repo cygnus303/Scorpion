975e0c6d7d2dca261df9801a054dd722dd042287
Archi <archivirani01@gmail.com>
2026-07-25 14:08:12 +0530

    user profile dropdown issue resolved

--- scorpion/src/app/components/vendor-portal-layout/vendor-header/vendor-header.component.ts
+++ scorpion/src/app/components/vendor-portal-layout/vendor-header/vendor-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild, viewChild } from '@angular/core';
+import { Component, ViewChild, HostListener, ElementRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { VendorLayoutService } from '../vendor-layout.service';
 import { VendorProfileComponent } from '../vendor-profile/vendor-profile.component';
@@ -12,12 +12,19 @@ import { VendorProfileComponent } from '../vendor-profile/vendor-profile.compone
 })
 export class VendorHeaderComponent {
   isUserMenuOpen = false;
-   @ViewChild(VendorProfileComponent) vendorProfileComponent!: VendorProfileComponent;
+  @ViewChild(VendorProfileComponent) vendorProfileComponent!: VendorProfileComponent;
   
+  constructor(private layoutService: VendorLayoutService, private eRef: ElementRef) {}
 
-  constructor(private layoutService: VendorLayoutService) {}
+  @HostListener('document:click', ['$event'])
+  clickout(event: Event) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.isUserMenuOpen = false;
+    }
+  }
 
-  toggleUserMenu() {
+  toggleUserMenu(event: Event) {
+    event.stopPropagation();
     this.isUserMenuOpen = !this.isUserMenuOpen;
   }
 
@@ -26,11 +33,12 @@ export class VendorHeaderComponent {
   }
 
   logout() {
+    this.isUserMenuOpen = false;
     console.log('Logout clicked');
   }
 
   openProfile(){
+    this.isUserMenuOpen = false;
     this.vendorProfileComponent.showPopup();
-
   }
 }
