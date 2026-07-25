c766b318abecd97eeae19bd0dcd2610a40e693ed
Archi <archivirani01@gmail.com>
2026-07-25 14:22:57 +0530

    debit note view print open

--- scorpion/src/app/components/vendor-portal-layout/invoice-tracker/invoice-tracker.component.ts
+++ scorpion/src/app/components/vendor-portal-layout/invoice-tracker/invoice-tracker.component.ts
@@ -113,4 +113,15 @@ export class InvoiceTrackerComponent {
     }
   }
 
+  openDebitView(debitNo: string){
+     const url = `${this.env.liveUrl}ViewPrint/ViewDebitNote?VoucherNo=${debitNo}&src=angular`;
+    const popup = window.open('', 'popupWindow',
+      'width=900,height=600,top=100,left=200,resizable=yes,scrollbars=yes'
+    );
+
+    if (popup) {
+      popup.location.href = url;
+    }
+  }
+
 }
